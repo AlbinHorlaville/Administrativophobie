d13be60673f9c9035ad4864dcb75a08f72adfea1
Albin Horlaville <98279992+AlbinHorlaville@users.noreply.github.com>
2025-03-25 10:34:43 -0400

    Boule de papier

--- game/Assets/Scenes/BasicScene/Script/Logic.cs
+++ game/Assets/Scenes/BasicScene/Script/Logic.cs
@@ -1,22 +1,41 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Logic : MonoBehaviour
 {
 
     public int score = 0;
     public TextMeshPro scoreMesh;
+    public InputActionReference makeABallAction;
+    public XRBaseInteractor interactor;
+    public GameObject BouleDePapier;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        makeABallAction.action.Enable();
+        makeABallAction.action.performed += MakeABall;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MakeABall(InputAction.CallbackContext context)
     {
-        
+        if (!interactor.hasSelection){
+            return;
+        }
+
+        GameObject heldObject = interactor.GetOldestInteractableSelected().transform.gameObject;
+        if (heldObject.name.Contains("Copy")){
+            // Détruire la copie
+            interactor.interactionManager.SelectExit(interactor, interactor.GetOldestInteractableSelected());
+            Destroy(heldObject);
+            
+            // Créer la boule de papier
+            GameObject boule = Instantiate(BouleDePapier);
+            XRGrabInteractable grabInteractable = boule.GetComponent<XRGrabInteractable>();
+            interactor.interactionManager.SelectEnter((IXRSelectInteractor)interactor, grabInteractable);
+        }
     }
 
     public void UpdateScore(int scoreToAdd)
